d15cc8637e16e0b342dc8424a92e9d7e6c5d0335
Sxentrie <jasonrico.jamora@gmail.com>
2025-09-08 17:21:45 +0800

    00011

--- index.tsx
+++ index.tsx
@@ -22,6 +22,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 // The application entry point, loading the main App component from the shell.
 import { App } from "./src/shell/app";
+import "./index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
